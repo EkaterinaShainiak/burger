bbddcf7e70ba4c50ccfa641a5f381731bfd54577
Katrina Shainiak <ekaterina.shainiak@gmail.com>
2017-12-12 07:29:08 -0800

    completed "get all" route in burger_controller.js

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,2 +1,12 @@
  var express = require("express");
- var burger = require("../models/burger.js")
+
+//   Import the model (burger.js) to use it's database functions.
+ var burger = require("../models/burger.js");
+
+module.exports = function(app) {
+app.get("/all", function(req, res){
+    burger.all(function(allBurgers){
+        res.send(allBurgers);
+    })
+})
+}
--- server.js
+++ server.js
@@ -4,12 +4,13 @@ var bodyParser = require("body-parser");
 var override = require("method-override");
 var orm = require("./config/orm");
 
-
-orm.selectAll();
-orm.updateOne(1);
-orm.insertOne("megaburger");
-
+// orm.selectAll();
+// orm.updateOne(1);
+// orm.insertOne("megaburger");
 
 var app = express();
+require("./controllers/burgers_controller")(app);
 
+app.use(bodyParser.json());
+app.use(express.static("public"));
 app.listen(process.env.PORT || 3000);
